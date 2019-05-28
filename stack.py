5263cb2860ea6326517be1e643ec4a9a6b8a6bfc
Hang Yang <hangyang96@163.com>
2019-05-28 20:00:42 +0800

    Update stack.py

--- stack.py
+++ stack.py
@@ -1,35 +1,3 @@
-# implement stack using arrays
-from sys import maxsize
-
-
-def createStack():
-    stack = []
-    return stack
-
-
-def isEmpty(stack):
-    return len(stack) == 0
-
-
-def push(stack, item):
-    stack.append(item)
-    print(item + "pushed to stack")
-
-
-def pop(stack):
-    if (isEmpty(stack)):
-        return str(-maxsize - 1)  # return minus infinite
-
-    return stack.pop()
-
-
-stack = createStack()
-push(stack, str(10))
-push(stack, str(20))
-push(stack, str(30))
-print(pop(stack) + "popped from stack")
-
-
 # implement stack using linked list
 class StackNode:
     def __init__(self, data):
@@ -64,3 +32,46 @@ stack.push(60)
 stack.push(40)
 stack.push(50)
 print("%d poped from stack" % (stack.pop()))
+
+
+# reverse the string using stack
+def createStack():
+    stack = []
+    return stack
+
+
+def size(stack):
+    return len(stack)
+
+
+def isEmpty(stack):
+    if len(stack) == 0:
+        return True
+
+
+def push(stack, item):
+    stack.append(item)
+    # print(item + "pushed to stack")
+
+
+def pop(stack):
+    if isEmpty(stack): return
+    return stack.pop()
+
+
+def reverse(string):
+    n = len(string)
+    stack = createStack()
+    for i in range(0, n, 1):
+        push(stack, string[i])
+    string = ""
+    for i in range(0, n, 1):
+        string += pop(stack)
+    return string
+
+
+string = "Geeks"
+string = reverse(string)
+print(string)
+
+
